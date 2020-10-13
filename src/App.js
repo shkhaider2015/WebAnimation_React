0dfd5b1c7ed7a0c1098ece48978a70396b186569
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2020-10-13 15:30:54 +0500

    dsfd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 import ReactTuto from "./components/reactTuto";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <ReactTuto />
     </div>
   );
--- src/components/reactTuto.js
+++ src/components/reactTuto.js
@@ -1,48 +1,52 @@
 import React, { useState } from 'react';
-import ReactCSSTransitionGroup from "react-transition-group";
-
+import styles from "../styles/reactTuto.css";
+import { CSSTransitionGroup } from "react-transition-group";
 
 
 
 function ReactTuto() {
-    const initItem = { items : ['Hello', 'World', 'Click', 'Me'] }
-    const [state, setState] = useState([initItem]);
-    console.log(state)
-    function handleAdd()
-    {
-        let txt = prompt('Enter Some Text');
-        setState({items : txt})
+    const initItem = { items: ['Hello', 'World', 'Click', 'Me'] }
+    const [state, setState] = useState(initItem);
+    console.log(state.items)
+    function handleAdd() {
+        let txt = state.items.concat([
+            prompt("Enter Text Here")
+        ])
+        setState({ items: txt })
     }
-    function handleRemove(i)
-    {
-       let newItem = state.items.slice();
-       newItem.splice(i, 1);
-       setState({items : newItem})
+    function handleRemove(i) {
+        let newItem = state.items.slice();
+        newItem.splice(i, 1);
+        setState({ items: newItem })
     }
-    
-    
+
+    const items = state.items.map(
+        (object, index) => {
+            return (<div key={index} onClick={() => { handleRemove(index) }} >
+                {object}
+            </div>)
+        }
+    )
+
     return (
         <div>
-            {
-                state.items.map(
-                    (object, index) => 
-                    {
-                        return (<div key={index} onClick={()=> {handleRemove(index)}} >
-                             {object}
-                             </div>)
-                    }
-                )
-            }
             <div>
                 <button onClick={handleAdd}>Add Item</button>
-                <ReactCSSTransitionGroup
-                transitionName="Example"
-                transitionEnterTimeout={500}
-                transitionLeaveTimeout={300}
+                <CSSTransitionGroup
+                    transitionName="example"
+                    transitionEnterTimeout={2000}
+                    transitionLeaveTimeout={2000}
                 >
-                    {state.items}
-                </ReactCSSTransitionGroup>
+                    <div style={{width:'200px', backgroundColor : 'green' }}>
+                        {
+                            items
+                        }
+                    </div>
+
+                </CSSTransitionGroup>
             </div>
+            <hr />
+            
         </div>
     );
 }
